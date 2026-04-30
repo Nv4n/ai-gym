28e6092c57b4583265435eeb75dd41fae575d197
Sybatron <vipsister2014@gmail.com>
2026-04-30 18:09:26 +0300

    api cleanup

--- app/api/products/route.ts
+++ app/api/products/route.ts
@@ -14,7 +14,6 @@ export async function GET(request: NextRequest) {
 	// }
 
 	const productsRes = await db.select().from(products);
-	console.log(productsRes);
 
 	return NextResponse.json(productsRes);
 }
